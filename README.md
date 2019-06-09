88a2f1ba914b3d169b71b0c604ac00b2056fde64
Andrés Marrugo <andrexmarrugo@gmail.com>
2019-06-09 18:35:21 -0500

    saving files now - now i'm watching the light in the tunnel's end

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# Arduino Plattform form analog sensor monitoring
+# Arduino plattform for analog sensor monitoring
 
 This project has the objective of reading and saving data from Arduino board analog pins
 
 ## Getting Started
 
-These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
 
 ### Prerequisites
 
@@ -25,6 +25,11 @@ npm start
 ## Built With
 
 * [Johnny Five](http://johnny-five.io/) - The JS Robotic framework used
+* [Electron](https://electronjs.org/) - Electron uses Chromium and Node.js so you can build your app with HTML, CSS, and JavaScrip
+* [Socket IO](https://socket.io/) -Socket.IO enables real-time, bidirectional and event-based communication.
+* [React JS](https://reactjs.org/) - A JavaScript library for building user interfaces
+
+
 
 
 ## Authors
--- src/client.js
+++ src/client.js
@@ -1,30 +1,34 @@
 const path = require('path')
 const five = require('johnny-five')
 const {ipcRenderer} = require('electron')
-import * as usb from 'usb'
+const fs = require('fs')
+const os = require('os')
 
 console.log('Iniciando')
+
 const globalBoard = new five.Board({repl: false})
 let boardState = false
 let portConnected = null
-
+let currentdate= new Date()
+var datetime = currentdate.getDay() + "-"+currentdate.getMonth() 
++ "-" + currentdate.getFullYear() + "@" 
++ currentdate.getHours() + "-" 
++ currentdate.getMinutes() + "-" + currentdate.getSeconds();
+const dirName = `LogSess-${datetime}`
 //console.log("argv:",process.argv)
 
-/**
- * Gestion del puerto USB
- */
-
-
-/* usb.on('detach', function (device) {
-  const port = 'COM' + device.portNumbers[0]
-  console.log('port off:', device.portNumbers[0])
-  console.log('board port:', portConnected || undefined)
-  if (port === portConnected || portConnected === undefined) {
-
-    device.close()
-    ipcRenderer.send('close')
+const folderDir = path.join(os.homedir(),'.iot-duino')
+console.log({folderDir})
+try {
+  if (!fs.existsSync(folderDir)){
+    fs.mkdirSync(folderDir)
   }
-}) */
+  if (!fs.existsSync(path.join(folderDir, dirName))){
+    fs.mkdirSync(path.join(folderDir, dirName))
+  }
+} catch (err) {
+  console.error(err)
+}
 
 globalBoard.on('ready', ()=>{
     boardState = globalBoard.isReady;
@@ -65,8 +69,17 @@ globalBoard.on('ready', ()=>{
     sensors.map((sen,index) => {
       sen.on('data', (data)=>{
         console.log('sensor'+sen.pin, data);
-        //mainWindow.webContents.send('sensor'+sen.pin, data);
-        ipcRenderer.send('data', { sensorPin: sen.pin, data: data})
+        let obj = { sensorPin: parseInt(sen.pin), data, date: currentdate.toLocaleString('en-US',{
+          day: 'numeric',
+          month: 'numeric',
+          year: 'numeric',
+          hour12: false,
+          hour: '2-digit',
+          minute: '2-digit',
+          second: '2-digit'
+        })}
+        insertInFile(obj)
+        ipcRenderer.send('data', obj)
       });
       
     });
@@ -85,3 +98,17 @@ globalBoard.on('ready', ()=>{
     
 
   })
+
+  
+
+  let insertInFile = (data) => {
+    // eslint-disable-next-line no-path-concat
+    try {
+      fs.appendFile(path.join(folderDir, `${dirName}/sensor${data.sensorPin}`), `${JSON.stringify(data)}\n`, (err)=>{
+      if(err) throw err
+      })
+    } catch (error) {
+      console.err({err})
+    }
+    
+  }
